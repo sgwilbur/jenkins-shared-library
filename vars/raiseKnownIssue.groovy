e3faa9b36f59a9182354aeace38b3bee4aa02f37
Sean G Wilbur <sgwilbur@gmail.com>
2019-11-26 09:03:42 -0600

    Adding resource lookup

--- vars/raiseKnownIssue.groovy
+++ vars/raiseKnownIssue.groovy
@@ -1,5 +1,10 @@
 import com.perficient.devops.log.*
 
 def call( String errorCode, String message=''){
+
+  issueResource = libraryResource('issues.yml')
+
+  echo "${issueResource}"
+
   echo PerficientMessage.knownIssue(errorCode, message )
 }
