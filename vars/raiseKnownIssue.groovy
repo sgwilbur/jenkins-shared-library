906ded21b60f0717e8bd9cc53ddf90d06d4ccea6
Sean G Wilbur <sgwilbur@gmail.com>
2019-11-26 10:13:59 -0600

    splitting load and pa rse of yaml file.

--- vars/raiseKnownIssue.groovy
+++ vars/raiseKnownIssue.groovy
@@ -3,18 +3,23 @@ import com.perficient.devops.log.*
 def call( String issueCode, String message=''){
 
   // load the issueMap from the resources file
-  issuesMap = readYaml text: libraryResource('issues.yml')
+  issuesResourceString = libraryResource('issues.yml')
+  issuesMap = readYaml text: issueResourceString
+
+  echo "issuesMap: ${issueMap}"
 
   // only raise issue if found
-  if( issuesMap.containsKey( 'issues' ) && issuesMap.errors.containsKey( issueCode ) ){
+  if( issuesMap && issuesMap.containsKey( 'issues' ) && issuesMap.errors.containsKey( issueCode ) ){
     issue = issuesMap.errors.get(issueCode)
     message = "${issueCode} - ${issue.message}"
-    echo PerficientMessage.log(message )
+    // determine if we need to warn or end execution
   }else{
     // when errorCode not found, degrade to standard error
     // append a message here to indicate a failed error code lookup
     message = "Error finding known issue ${issueCode} degrading to standard error. " + message
-    echo PerficientMessage.error(message)
   }
+
+  echo PerficientMessage.error(message )
   error(message)
+
 }
